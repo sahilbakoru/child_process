dda76f1970269a468ca4edd570abac7ac134e110
sahilbakoru <sahilbakoru1999@gmail.com>
2023-09-25 19:20:11 +0530

    little formatting .

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-var {exec, spawn} = require('child_process')
-
-
+var { exec, spawn } = require("child_process");
 
 function DownloadModule() {
-    exec('npm i react-native-option-dropdown', function(err, stdout,stderr){
-        if(err) console.error(stderr);
-        console.log(stdout);
-    });
-
+  exec("npm i react-native-option-dropdown", function (err, stdout, stderr) {
+    if (err) console.error(stderr);
+    console.log(stdout);
+  });
 }
 function DeleteModule() {
-     exec('npm uni react-native-option-dropdown', function(err, stdout,stderr){
-        if(err) console.error(stderr);
-        console.log(stdout);
-    });
+  exec("npm uni react-native-option-dropdown", function (err, stdout, stderr) {
+    if (err) console.error(stderr);
+    console.log(stdout);
+  });
 }
-
 
 setInterval(DownloadModule, 240000);
 setInterval(DeleteModule, 250000);
