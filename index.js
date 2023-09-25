e1a6f827b17c50ef98d541130ffb5550c630df06
sahilbakoru <68190549+sahilbakoru@users.noreply.github.com>
2023-09-25 19:14:39 +0530

    Update index.js to add delete code
    Update index.js to add delete code

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ function DownloadModule() {
         if(err) console.error(stderr);
         console.log(stdout);
     });
+
+}
+function DeleteModule() {
+     exec('npm uni react-native-option-dropdown', function(err, stdout,stderr){
+        if(err) console.error(stderr);
+        console.log(stdout);
+    });
 }
 
 
 setInterval(DownloadModule, 240000);
+setInterval(DeleteModule, 250000);
